1dbcb2f3b727d41b1ddde5b7942113b28001c855
Diego Borja <diego.borja.mail@gmail.com>
2023-12-01 18:05:11 +0100

    [Tarea-2] Desarrollar más pruebas para `AdditionService`.

--- src/test/java/com/sanitas/test4/calculator/service/AdditionServiceTest.java
+++ src/test/java/com/sanitas/test4/calculator/service/AdditionServiceTest.java
@@ -1,26 +1,44 @@
 package com.sanitas.test4.calculator.service;
 
-import org.junit.jupiter.api.DisplayName;
+import com.sanitas.test4.calculator.exception.InvalidOperationException;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import java.math.BigDecimal;
 import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
+@SpringBootTest
 public class AdditionServiceTest {
 
-    private final AdditionService additionService = new AdditionService();
+    @Autowired
+    private AdditionService additionService;
 
     @Test
-    @DisplayName("1 + 4 + 5  = 10")
-    void shouldAddNumbersCorrectly() {
+    void additionThreeNumbers() {
         List<BigDecimal> numbers = Arrays.asList(BigDecimal.valueOf(1), BigDecimal.valueOf(4), BigDecimal.valueOf(5));
         BigDecimal result = this.additionService.performOperation(numbers);
-        assertEquals(BigDecimal.valueOf(10), result, "1 + 4 + 5  should equal 10");
+        assertEquals(BigDecimal.valueOf(10), result, "1 + 4 + 5 should equal 10");
+    }
+
+    @Test
+    void additionNegativeNumbers() {
+        List<BigDecimal> numbers = Arrays.asList(BigDecimal.valueOf(-1), BigDecimal.valueOf(-4), BigDecimal.valueOf(-5));
+        BigDecimal result = this.additionService.performOperation(numbers);
+        assertEquals(BigDecimal.valueOf(-10), result, "(-1) + (-4) + (-5) should equal -10");
+    }
+
+    @Test
+    void additionZeroAndPositiveNumber() {
+        List<BigDecimal> numbers = Arrays.asList(BigDecimal.valueOf(0), BigDecimal.valueOf(7));
+        BigDecimal result = this.additionService.performOperation(numbers);
+        assertEquals(BigDecimal.valueOf(7), result, "0 + 7 should equal 7");
     }
 
     @ParameterizedTest(name = "{0} + {1} = {2}")
@@ -30,9 +48,16 @@ public class AdditionServiceTest {
             "48,  52, 100",
             "2,  99, 101"
     })
-    void add(int first, int second, int expectedResult) {
+    void shouldAddNumbersCorrectly(int first, int second, int expectedResult) {
         List<BigDecimal> numbers = Arrays.asList(BigDecimal.valueOf(first), BigDecimal.valueOf(second));
         BigDecimal result = this.additionService.performOperation(numbers);
         assertEquals(BigDecimal.valueOf(expectedResult), result, () -> first + " + " + second + " should equal " + expectedResult);
     }
+
+    @Test
+    void shouldThrowExceptionWithEmptyList() {
+        List<BigDecimal> numbers = List.of();
+        assertThrows(InvalidOperationException.class, () -> this.additionService.performOperation(numbers));
+    }
+
 }
