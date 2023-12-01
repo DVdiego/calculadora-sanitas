1e67796097239130c45aff75b372b14ec4a25b48
Diego Borja <diego.borja.mail@gmail.com>
2023-12-01 18:07:01 +0100

    [Tarea-2] Añadir validación cuando la lista está vacia.

--- src/main/java/com/sanitas/test4/calculator/service/AdditionService.java
+++ src/main/java/com/sanitas/test4/calculator/service/AdditionService.java
@@ -1,11 +1,17 @@
 package com.sanitas.test4.calculator.service;
 
+import com.sanitas.test4.calculator.exception.InvalidOperationException;
+import org.springframework.stereotype.Component;
+
 import java.math.BigDecimal;
 import java.util.List;
-
+@Component
 public class AdditionService implements OperationService {
     @Override
     public BigDecimal performOperation(List<BigDecimal> numbers) {
+        if (numbers.isEmpty()) {
+            throw new InvalidOperationException("The list of numbers cannot be empty for addition.");
+        }
         return numbers.stream().reduce(BigDecimal.ZERO, BigDecimal::add);
     }
 
