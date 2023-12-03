fa141ffea3f27d0e1892d14a57d0a02c38e1f860
Diego Borja <diego.borja.mail@gmail.com>
2023-12-03 18:58:11 +0100

    [Tarea-3] Implementar `BasicOperationRequest` para que pase las pruebas

--- src/main/java/com/sanitas/test4/calculator/model/BasicOperationRequest.java
+++ src/main/java/com/sanitas/test4/calculator/model/BasicOperationRequest.java
@@ -1,4 +1,14 @@
 package com.sanitas.test4.calculator.model;
 
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+@Data
 public class BasicOperationRequest {
+
+    private String operation;
+    private List<BigDecimal> numbers;
+
 }
