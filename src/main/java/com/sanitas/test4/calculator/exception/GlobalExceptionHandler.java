a8b0879264857f151c86b478c73722cac818d42a
Diego Borja <diego.borja.mail@gmail.com>
2023-12-03 20:18:38 +0100

    [Tarea-5] Añadir más métodos para capturar todas las excepciones con la librería de traceo (`TracerAPI`).

--- src/main/java/com/sanitas/test4/calculator/exception/GlobalExceptionHandler.java
+++ src/main/java/com/sanitas/test4/calculator/exception/GlobalExceptionHandler.java
@@ -5,6 +5,7 @@ import io.corp.calculator.TracerAPI;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -18,8 +19,22 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(InvalidOperationException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ResponseEntity<String> handleInvalidOperation(InvalidOperationException ex) {
-        tracerAPI.trace("Exception InvalidOperationException: " + ex.getMessage());
+        this.tracerAPI.trace("Exception InvalidOperationException: " + ex.getMessage());
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ResponseEntity<String> handleHttpMessageNotReadableException(HttpMessageNotReadableException ex) {
+        this.tracerAPI.trace("Exception HttpMessageNotReadableException: " + ex.getMessage());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
+    }
+
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ResponseEntity<String> handleException(Exception ex) {
+        this.tracerAPI.trace("Internal Server Error: " + ex.getMessage());
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Internal Server Error");
     }
 
 }
