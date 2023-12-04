c29c6ff241d7f4b41bef09d54c28f35c8ab717df
Diego Borja <diego.borja.mail@gmail.com>
2023-12-04 15:27:53 +0100

    [Tarea-3] Añadir información adicional sobre la estructura y el formato de los campos

--- src/main/java/com/sanitas/test4/calculator/model/BasicOperationRequest.java
+++ src/main/java/com/sanitas/test4/calculator/model/BasicOperationRequest.java
@@ -1,5 +1,6 @@
 package com.sanitas.test4.calculator.model;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
 import java.math.BigDecimal;
@@ -8,7 +9,15 @@ import java.util.List;
 @Data
 public class BasicOperationRequest {
 
+    @Schema(
+            description = "Type of operation to perform. Supported values: '+', '-'",
+            example = "+"
+    )
     private String operation;
+    @Schema(
+            description = "List of numbers to operate on.",
+            example = "[2, 3, 5]"
+    )
     private List<BigDecimal> numbers;
 
 }
