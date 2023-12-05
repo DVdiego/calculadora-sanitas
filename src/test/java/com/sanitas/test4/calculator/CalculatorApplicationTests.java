d3bdaaa68f2c8de1e31c37c734481d05d5756d70
Diego Borja <diego.borja.mail@gmail.com>
2023-12-05 09:30:33 +0100

    [Tarea-7] Pruebas de unitarias para superar un 80% de cobertura de código

--- src/test/java/com/sanitas/test4/calculator/CalculatorApplicationTests.java
+++ src/test/java/com/sanitas/test4/calculator/CalculatorApplicationTests.java
@@ -1,13 +1,24 @@
 package com.sanitas.test4.calculator;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.system.CapturedOutput;
+import org.springframework.boot.test.system.OutputCaptureExtension;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
+@ExtendWith(OutputCaptureExtension.class)
 class CalculatorApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
 
+	@Test
+	void testMain(CapturedOutput output) {
+		CalculatorApplication.main(new String[]{});
+		assertThat(output).contains("Started CalculatorApplication");
+	}
 }
