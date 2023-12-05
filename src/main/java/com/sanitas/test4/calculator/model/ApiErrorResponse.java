d57aa6ef5449ad5f5ac7365ea9b4e6993be85daf
Diego Borja <diego.borja.mail@gmail.com>
2023-12-05 09:25:29 +0100

    [Tarea-5] Añadir Schema mensaje para errores

--- src/main/java/com/sanitas/test4/calculator/model/ApiErrorResponse.java
+++ src/main/java/com/sanitas/test4/calculator/model/ApiErrorResponse.java
@@ -6,6 +6,8 @@ import lombok.Data;
 @Data
 @Schema(description = "Object representing the standard error for the API.")
 public class ApiErrorResponse {
+    @Schema(description = "Error Message")
     private String message;
+    @Schema(description = "Error description")
     private String details;
 }
