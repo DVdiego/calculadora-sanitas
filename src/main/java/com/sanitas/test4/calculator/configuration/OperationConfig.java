a44b126431b17b9a22f2cc75f177a1dc9a0cb6e2
Diego Borja <diego.borja.mail@gmail.com>
2023-12-13 23:54:28 +0100

    [Tarea-2] Cambiar inyección de dependencias mediante constructor

--- src/main/java/com/sanitas/test4/calculator/configuration/OperationConfig.java
+++ src/main/java/com/sanitas/test4/calculator/configuration/OperationConfig.java
@@ -1,5 +1,6 @@
 package com.sanitas.test4.calculator.configuration;
 
+import com.sanitas.test4.calculator.exception.ExceptionMessages;
 import com.sanitas.test4.calculator.model.OperationProvider;
 import com.sanitas.test4.calculator.service.AdditionService;
 import com.sanitas.test4.calculator.service.OperationService;
@@ -14,17 +15,23 @@ import java.util.Map;
 @Configuration
 public class OperationConfig implements OperationProvider {
 
+    private final ExceptionMessages exceptionMessages;
+
     @Value("${operation.supported.addition}")
     private String addition;
 
     @Value("${operation.supported.subtraction}")
     private String subtraction;
 
+    public OperationConfig(ExceptionMessages exceptionMessages) {
+        this.exceptionMessages = exceptionMessages;
+    }
+
     @Bean
     public Map<String, OperationService> getOperations() {
         Map<String, OperationService> operations = new HashMap<>();
-        operations.put(this.addition, new AdditionService());
-        operations.put(this.subtraction, new SubtractionService());
+        operations.put(this.addition, new AdditionService(exceptionMessages));
+        operations.put(this.subtraction, new SubtractionService(exceptionMessages));
         return operations;
     }
 }
